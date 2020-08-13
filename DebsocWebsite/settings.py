52d92a3c12f5d5c12c9b9282e240e9af818d3124
rishav4101 <rajkumarrishav4101@gmail.com>
2020-08-13 17:11:53 +0530

    Profile entry Temporary fixes

--- DebsocWebsite/settings.py
+++ DebsocWebsite/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = ['localhost','127.0.0.1','debsocnitd.team']
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'whitenoise.runserver_nostatic',
     'django.contrib.admin',
@@ -154,8 +153,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
+# SESSION_COOKIE_SECURE = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
