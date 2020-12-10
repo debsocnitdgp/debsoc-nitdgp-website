4c513d4edb3a80977e6933439d36d3856565fc1f
rishav4101 <rajkumarrishav4101@gmail.com>
2020-12-10 07:24:28 +0530

    migration added test 01

--- DebsocWebsite/local_settings.py
+++ DebsocWebsite/local_settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'y&z($el+d%sp8foi24e_=ng&m*v^h@nrbm3@4k=8)*i#o5uyco'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -168,9 +168,9 @@ MEDIA_URL = '/media/'
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': 'hdqtmtoze',
-    'API_KEY': os.environ.get('API_SEC'),
-    'API_SECRET': os.environ.get('API_KEY'),
+    'API_KEY': '862579851327152',
+    'API_SECRET': 'zhEZix-M3dQ9ZVzabksRKSsSr_k',
 }
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+#DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 django_heroku.settings(locals())
