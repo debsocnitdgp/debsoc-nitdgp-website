45718c057af46c043fa220a154a642561180df0a
Kaustuv942 <kaustuv942@gmail.com>
2020-12-16 19:51:36 +0530

    updated requirements.txt

--- DebsocWebsite/urls.py
+++ DebsocWebsite/urls.py
@@ -26,6 +26,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('', include('sitewebapp.urls')),
     path('login/', TemplateView.as_view(template_name="social_app/index.html")),
+    path('tour/', include('Tour.urls')),
     path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
