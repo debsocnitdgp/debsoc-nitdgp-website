4afe5f609ac3148e8aff5c456fdab9384a43d3f7
Kaustuv <kaustuv942@gmail.com>
2021-02-01 04:43:46 +0530

    redirecting to https

--- DebsocWebsite/settings.py
+++ DebsocWebsite/settings.py
@@ -164,9 +164,12 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-# CSRF_COOKIE_SECURE = True
-# SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+PREPEND_WWW = True
+BASE_URL = "https://www.debsocnitdgp.in"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
