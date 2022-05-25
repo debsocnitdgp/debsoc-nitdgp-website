1661d37461aeaa10c8223ef76e78174bd5332a32
Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>
2022-05-25 11:22:27 +0530

    remove unused imports from `sitewebapp`
    Signed-off-by: Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>

--- DebsocWebsite/urls.py
+++ DebsocWebsite/urls.py
@@ -17,8 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.defaults import page_not_found
-from django.conf.urls import url
 
 
 
