5060baaae93190e513a07b791b2e5d78dd6934ec
Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>
2022-06-03 16:13:02 +0530

    write serializers for existing models
    Signed-off-by: Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>

---
+++ sitewebapp/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+from .models import Members, blog as Blogs, Comments, event as Events, Alumni, access_tokens
+# , Candidates, auditionAnswers, auditionQuestions, auditionRounds
+
+
+class MemberSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Members
+        fields = ['username', 'firstname', 'lastname', 'email', 'bio', 'year', 'post', 'sno', 'dp', 'facebook_url', 'instagram_url', 'linkedin_url']
+
+class BlogSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Blogs
+        fields = ['title', 'blog_text', 'image_url', 'created_on', 'active', 'author']
+
+class CommentsSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Comments
+        fields = ['post', 'comment_by', 'comment', 'commented_on', 'active']
+
+class EventSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Events
+        fields = ['event_name', 'event_description', 'poster', 'event_datetime', 'event_mode', 'event_status', 'active', 'text1', 'url1', 'text2', 'url2', 'text3', 'url3']
+
+class AlumniSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Alumni
+        fields = ['firstname', 'lastname', 'email', 'batch', 'sno', 'propic', 'facebook_url', 'instagram_url', 'linkedin_url']
+
+class AccessTokensSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = access_tokens
+        fields = ['name', 'value']
